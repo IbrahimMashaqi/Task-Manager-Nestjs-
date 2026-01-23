cbc57a399d12d6644286c7819fa77f03a8b077e5
Ibrahim Mashaqi <ibrahimmashaqi04@gmail.com>
2026-01-23 20:22:55 +0200

    readme update

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,43 @@ import {
   ValidationPipe,
   Logger,
 } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // 1. استيراد السواجر
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(process.env.PORT ?? 3000);
-  logger.log('Application listening on port 3000');
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription(
+      'The official documentation for Ibrahim Mashaqi Task Management API',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+  logger.log(
+    `Swagger documentation available at: http://localhost:${port}/api`,
+  );
 }
 bootstrap();
