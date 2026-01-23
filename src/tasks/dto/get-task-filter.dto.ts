25841c7a8d6e33bfe1591ba845ea084645c9dd01
Ibrahim Mashaqi <ibrahimmashaqi04@gmail.com>
2026-01-23 19:14:46 +0200

    case insesietive in enum

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,8 +1,10 @@
+import { Transform } from 'class-transformer';
 import { TaskStatus } from '../task-status.enum';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class GetTaskFilterDto {
   @IsOptional()
+  @Transform(({ value }) => value?.toUpperCase())
   @IsEnum(TaskStatus)
   status?: TaskStatus;
 
